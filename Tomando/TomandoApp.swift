3ed837510e9a2812037b30e5205089aa6b3ae7ed
juandahurt <juandahurtadom@gmail.com>
2020-12-24 11:21:38 -0500

    Initial Commit

---
+++ Tomando/TomandoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TomandoApp.swift
+//  Tomando
+//
+//  Created by juandahurt on 24/12/20.
+//
+
+import SwiftUI
+
+@main
+struct TomandoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
